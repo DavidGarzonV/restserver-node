ff552f5882eae7137320d07cd986c15aba5e8f41
David <dgv241198@gmail.com>
2018-09-10 16:06:26 -0500

    Primer Commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// =================================
+// Puerto
+// =================================
+        //Puerto definido para la aplicacion, sino está, se usa el 3000
+process.env.PORT = process.env.PORT || 3000;
